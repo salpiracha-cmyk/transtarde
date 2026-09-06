10f9fb03df32aa1b45a1e001397dbf50bfede2c9
salpiracha-cmyk <salpiracha@gmail.com>
2026-09-07 01:29:37 +0500

    Add secure login: module.php

---
+++ module.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+require __DIR__ . '/auth_store.php';
+tt_require_login();
+$modules = [
+    'milling' => __DIR__ . '/milling/Transtrade_Master_Milling_V3_3_2_AUDITED.html',
+    'exports' => __DIR__ . '/exports/Transtrade_Exports_Master_Prototype_V2_6_Final_Stabilized.html',
+];
+$id = strtolower((string)($_GET['id'] ?? ''));
+if (!isset($modules[$id]) || !is_file($modules[$id])) { http_response_code(404); exit('Module not found.'); }
+header('Content-Type: text/html; charset=UTF-8');
+readfile($modules[$id]);
