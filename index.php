b0ef6bfe2b2eb5f6a2a033a3dadeb938cd4f8d57
salpiracha-cmyk <salpiracha@gmail.com>
2026-09-07 23:42:20 +0500

    Expose Accounts module from Super Admin on feature branch

--- index.php
+++ index.php
@@ -16,4 +16,5 @@ $session = [
     'permissions'=>$user['permissions'] ?? [], 'csrf'=>tt_csrf(),
 ];
 $bootstrap = '<script>window.TT_SESSION=' . json_encode($session, JSON_HEX_TAG|JSON_HEX_AMP|JSON_HEX_APOS|JSON_HEX_QUOT) . ';</script>';
-echo str_replace('<script src="admin/app.js?v=20260907-3"></script>', $bootstrap . '<script src="admin/app.js?v=20260907-3"></script>', (string)$html);
+$replace = $bootstrap . '<script src="admin/app.js?v=20260907-3"></script><script src="admin/accounts-override.js?v=20260907-1"></script>';
+echo str_replace('<script src="admin/app.js?v=20260907-3"></script>', $replace, (string)$html);
