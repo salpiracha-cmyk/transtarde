d6d9fa1e1c3fb458c588cb1e96fa8dcbef0f2691
salman <salpiracha@gmail.com>
2026-09-12 16:41:15 +0500

    Refresh Accounts Soda launcher cache

--- accounts/index.php
+++ accounts/index.php
@@ -29,6 +29,6 @@ $access = [
 ];
 $bootstrap = '<script>window.TT_ACCOUNT_ACCESS=' . json_encode($access, JSON_HEX_TAG|JSON_HEX_AMP|JSON_HEX_APOS|JSON_HEX_QUOT) . ';</script>';
 $html = preg_replace('/<head(\s[^>]*)?>/i', '$0' . $bootstrap, $html, 1) ?? $html;
-$html = preg_replace('/<\/body>/i', '<script src="app-bundle.php?v=20260912-6"></script></body>', $html, 1) ?? $html;
+$html = preg_replace('/<\/body>/i', '<script src="app-bundle.php?v=20260912-7"></script></body>', $html, 1) ?? $html;
 echo $html;
 
